d28165ca551d4cf70f2914df1b6122a0e81beb5e
Venkat Gupta <venkatgupta@Venkats-MacBook-Air.local>
2023-12-30 00:05:30 +0530

    initial commit - server setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# "Bill Book Full Stack Web Application"
+
+1. Node js, Express js code setup
+
+**Description:**
+
+This repository shares the code developed for the bill book MERN Application
---
+++ controllers/registerController.js
@@ -0,0 +1,23 @@
+const User = require('../model/User');
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res.status(400).json({ message: 'Username and pwd are required' });
+  const duplicate = await User.findOne({ username: user }).exec();
+  if (duplicate) {
+    res.sendStatus(409);
+  }
+  try {
+    //encrypt the pwd
+    const hashedPwd = await bcrypt.hash(pwd, 10);
+    const result = await User.create({ username: user, password: hashedPwd });
+    console.log(result);
+    res.status(201).json({ message: `${user} has been created` });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { handleNewUser };
